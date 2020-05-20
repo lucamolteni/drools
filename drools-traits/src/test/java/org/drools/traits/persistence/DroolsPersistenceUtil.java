3ccfbc0385859bba6c178b6f6dd8e8c088260557
Luca Molteni <volothamp@gmail.com>
2020-05-20 10:52:15 +0200

    Moved trait persistence test to traits module

--- drools-traits/src/test/java/org/drools/traits/persistence/DroolsPersistenceUtil.java
+++ drools-traits/src/test/java/org/drools/traits/persistence/DroolsPersistenceUtil.java
@@ -13,13 +13,12 @@
  * limitations under the License.
 */
 
-package org.drools.persistence.util;
-
-import static org.kie.api.runtime.EnvironmentName.GLOBALS;
-import static org.kie.api.runtime.EnvironmentName.TRANSACTION;
+package org.drools.traits.persistence;
 
 import java.util.Map;
+
 import javax.transaction.UserTransaction;
+
 import org.drools.core.base.MapGlobalResolver;
 import org.drools.core.impl.EnvironmentFactory;
 import org.kie.api.runtime.Environment;
@@ -27,13 +26,20 @@ import org.kie.test.util.db.PersistenceUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.kie.api.runtime.EnvironmentName.ENTITY_MANAGER_FACTORY;
+import static org.kie.api.runtime.EnvironmentName.GLOBALS;
+import static org.kie.api.runtime.EnvironmentName.TRANSACTION;
+
 public class DroolsPersistenceUtil extends PersistenceUtil {
+
+    private static Logger logger = LoggerFactory.getLogger(DroolsPersistenceUtil.class);
 
     // Persistence and data source constants
     public static final String DROOLS_PERSISTENCE_UNIT_NAME = "org.drools.persistence.jpa";
 
     public static String OPTIMISTIC_LOCKING = "optimistic";
     public static String PESSIMISTIC_LOCKING = "pessimistic";
+
 
     public static Environment createEnvironment(Map<String, Object> context) {
         Environment env = EnvironmentFactory.newEnvironment();
