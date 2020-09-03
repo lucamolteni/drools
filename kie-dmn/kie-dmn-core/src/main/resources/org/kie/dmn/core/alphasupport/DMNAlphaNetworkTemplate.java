727dd7dd5f3a091e3aaec8b414f62e3d504dbe06
Luca Molteni <volothamp@gmail.com>
2020-09-03 11:51:04 +0200

    Always generate the same alpha node

--- kie-dmn/kie-dmn-core/src/main/resources/org/kie/dmn/core/alphasupport/DMNAlphaNetworkTemplate.java
+++ kie-dmn/kie-dmn-core/src/main/resources/org/kie/dmn/core/alphasupport/DMNAlphaNetworkTemplate.java
@@ -9,6 +9,7 @@ import java.lang.Override;
 
 import org.drools.compiler.builder.impl.KnowledgeBuilderImpl;
 import org.drools.core.reteoo.AlphaNode;
+import org.drools.core.reteoo.compiled.CompiledNetwork;
 import org.drools.model.Index;
 import org.kie.dmn.core.compiler.alphanetbased.DMNCompiledAlphaNetwork;
 import org.kie.dmn.core.compiler.alphanetbased.NetworkBuilderContext;
@@ -20,11 +21,35 @@ import static org.drools.compiler.reteoo.compiled.ObjectTypeNodeCompiler.compile
 import static org.kie.dmn.core.compiler.alphanetbased.AlphaNetworkCompilerUtils.addResultSink;
 import static org.kie.dmn.core.compiler.alphanetbased.AlphaNetworkCompilerUtils.createAlphaNode;
 import static org.kie.dmn.core.compiler.alphanetbased.AlphaNetworkCompilerUtils.createIndex;
+import static org.kie.dmn.feel.codegen.feel11.CompiledFEELSemanticMappings.gracefulEq;
 
 // All implementations are used only for templating purposes and should never be called
 public class DMNAlphaNetworkTemplate implements DMNCompiledAlphaNetwork {
 
     protected final ResultCollector resultCollector = new ResultCollector();
+    protected CompiledNetwork compiledNetwork;
+
+    public static final org.kie.dmn.feel.runtime.UnaryTest UT1 = (feelExprCtx, left) -> gracefulEq(feelExprCtx, "false", left);
+
+    public DMNAlphaNetworkTemplate() {
+
+        NetworkBuilderContext ctx = new NetworkBuilderContext();
+
+        Index index1 = createIndex(String.class, x -> (String)x.getValue(0), "false");
+
+
+        // Alpha network creation statements
+        {
+
+        }
+
+        Index index3 = createIndex(String.class, x -> (String)x.getValue(0), "dummy");
+        AlphaNode alphaDummy = createAlphaNode(ctx, ctx.otn, x -> false, index3);
+        addResultSink(ctx, this, alphaDummy, "DUMMY");
+
+        this.compiledNetwork = compile(new KnowledgeBuilderImpl(ctx.kBase), ctx.otn);
+        this.compiledNetwork.setObjectTypeNode(ctx.otn);
+    }
 
     @Override
     public Object evaluate(EvaluationContext evalCtx) {
