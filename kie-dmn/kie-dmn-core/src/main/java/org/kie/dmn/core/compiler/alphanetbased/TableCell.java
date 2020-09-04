fe6f6c9a779307255f95ace6f892ee4acfa86262
Luca Molteni <volothamp@gmail.com>
2020-09-04 08:52:35 +0200

    Alpha network generation

--- kie-dmn/kie-dmn-core/src/main/java/org/kie/dmn/core/compiler/alphanetbased/TableCell.java
+++ kie-dmn/kie-dmn-core/src/main/java/org/kie/dmn/core/compiler/alphanetbased/TableCell.java
@@ -3,18 +3,26 @@ package org.kie.dmn.core.compiler.alphanetbased;
 import java.util.Map;
 
 import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.expr.AssignExpr;
 import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.expr.StringLiteralExpr;
+import com.github.javaparser.ast.expr.VariableDeclarationExpr;
 import com.github.javaparser.ast.stmt.BlockStmt;
-import com.github.javaparser.ast.stmt.Statement;
-import org.kie.dmn.api.core.DMNType;
 import org.kie.dmn.core.compiler.DMNCompilerContext;
 import org.kie.dmn.core.compiler.DMNFEELHelper;
 import org.kie.dmn.core.compiler.execmodelbased.DTableModel;
+import org.kie.dmn.feel.codegen.feel11.CodegenStringUtil;
 import org.kie.dmn.feel.lang.Type;
 
+import static com.github.javaparser.StaticJavaParser.parseExpression;
+
 public class TableCell {
+
+    private static final String CREATE_ALPHA_NODE_METHOD = "org.kie.dmn.core.compiler.alphanetbased.AlphaNetworkCompilerUtils.createAlphaNode";
 
     public static class TableCellFactory {
 
@@ -31,12 +39,12 @@ public class TableCell {
         }
     }
 
-    private String input;
+    private final String input;
     private final DMNFEELHelper feel;
     private final DMNCompilerContext ctx;
-    private TableIndex tableIndex;
-    private String columnName;
-    private Type type;
+    private final TableIndex tableIndex;
+    private final String columnName;
+    private final Type type;
 
     private TableCell(DMNFEELHelper feel,
                       DMNCompilerContext ctx,
@@ -51,15 +59,29 @@ public class TableCell {
         this.type = columnModel.getType();
     }
 
-    public void addAlphaNetwork(BlockStmt stmt) {
+    public void addAlphaNetworkNode(BlockStmt stmt) {
+        com.github.javaparser.ast.type.Type alphaNodeType = StaticJavaParser.parseType("org.drools.core.reteoo.AlphaNode");
+        VariableDeclarationExpr variable = new VariableDeclarationExpr(alphaNodeType, tableIndex.appendTableIndexSuffix("alphaNode"));
 
-        Statement expr = StaticJavaParser.parseStatement(
-                String.format("AlphaNode alpha%s = createAlphaNode(ctx, ctx.otn, \"false\", x -> UT1.apply(x.getEvalCtx(), x.getValue(0)), index1);", tableIndex.getStringIndex())
-        );
+        // This is used for Alpha Sharing. It needs to have the column name to avoid collisions with same test in other cells
+        String constraintIdentifier = CodegenStringUtil.escapeIdentifier(columnName + input);
 
-//        Expression targetExpr = null;
-//        Expression createAlphaNodeExpr = null;
-//        Expression newAlphaNodeExpression = new AssignExpr(targetExpr, createAlphaNodeExpr, AssignExpr.Operator.ASSIGN);
+        Expression objectSource;
+        if(tableIndex.isFirstColumn()) {
+            objectSource = parseExpression("ctx.otn");
+        } else {
+            objectSource = new NameExpr(tableIndex.previousColumn().appendTableIndexSuffix("alphaNode"));
+        }
+
+        Expression alphaNodeCreation = new MethodCallExpr(null, CREATE_ALPHA_NODE_METHOD, NodeList.nodeList(
+                new NameExpr("ctx"),
+                objectSource,
+                new StringLiteralExpr(constraintIdentifier),
+                parseExpression("x -> UT1.apply(x.getEvalCtx(), x.getValue(0))"),
+                new NameExpr("index1")
+
+        ));
+        final Expression expr = new AssignExpr(variable, alphaNodeCreation, AssignExpr.Operator.ASSIGN);
         stmt.addStatement(expr);
     }
 
@@ -70,7 +92,7 @@ public class TableCell {
                 type,
                 false);
 
-        String className = String.format("UnaryTest%s", tableIndex.getStringIndex());
+        String className = tableIndex.appendTableIndexSuffix("UnaryTest");
         sourceCode.setName(className);
         allClasses.put(String.format("%s", className), sourceCode.toString());
     }
