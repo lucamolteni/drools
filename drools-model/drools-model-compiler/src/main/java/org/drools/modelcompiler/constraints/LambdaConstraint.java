5f200988e2fa94e22cd9149d93eebffa5a80ac6e
Luca Molteni <volothamp@gmail.com>
2020-10-30 12:48:36 +0100

    Overload for LambdaConstraint

--- drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/constraints/LambdaConstraint.java
+++ drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/constraints/LambdaConstraint.java
@@ -60,6 +60,10 @@ public class LambdaConstraint extends AbstractConstraint {
         initIndexes();
     }
 
+    public LambdaConstraint(ConstraintEvaluator evaluator) {
+        this(evaluator, PredicateInformation.EMPTY_PREDICATE_INFORMATION);
+    }
+
     private void initIndexes() {
         Index index = evaluator.getIndex();
         if (index != null) {
