20c6719e0a04b7fe09ca050fd973a0710a7071f4
Luca Molteni <volothamp@gmail.com>
2021-01-11 11:20:09 +0100

    Parametrized letter

--- drools-alphanetwork-compiler/src/test/java/org/drools/ancompiler/LargeAlphaNetworkTest.java
+++ drools-alphanetwork-compiler/src/test/java/org/drools/ancompiler/LargeAlphaNetworkTest.java
@@ -16,27 +16,11 @@
 
 package org.drools.ancompiler;
 
-import java.io.Serializable;
-import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.List;
 
 import org.assertj.core.api.Assertions;
-import org.drools.core.common.NamedEntryPoint;
-import org.drools.core.reteoo.CompositeObjectSinkAdapter;
-import org.drools.core.reteoo.ObjectSinkPropagator;
-import org.drools.core.reteoo.ObjectTypeNode;
 import org.junit.Test;
-import org.kie.api.KieServices;
-import org.kie.api.builder.ReleaseId;
-import org.kie.api.builder.model.KieModuleModel;
-import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-import org.kie.internal.builder.conf.AlphaNetworkCompilerOption;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 public class LargeAlphaNetworkTest extends BaseModelTest {
 
@@ -46,31 +30,14 @@ public class LargeAlphaNetworkTest extends BaseModelTest {
 
     @Test
     public void testVeryLargeAlphaNetwork() {
-        final String str =
-                "global java.util.List results;\n" +
-                        "import " + Person.class.getCanonicalName() + ";\n" +
-                        "rule minus18 when\n" +
-                        "    $p : Person( name == \"a\", age < 18 )\n" +
-                        "then\n" +
-                        " results.add($p);\n" +
-                        "end\n" +
-                        "rule between1825 when\n" +
-                        "    $p : Person( name == \"a\", age >= 18, age <= 25 )\n" +
-                        "then\n" +
-                        " results.add($p);\n" +
-                        "end\n" +
-                        "rule between2530 when\n" +
-                        "    $p : Person( name == \"a\", age >= 25, age <= 30 )\n" +
-                        "then\n" +
-                        " results.add($p);\n" +
-                        "end\n" +
-                        "rule greaterThan30 when\n" +
-                        "    $p : Person( name == \"a\", age > 30 )\n" +
-                        "then\n" +
-                        " results.add($p);\n" +
-                        "end\n";
+        final StringBuilder rule =
+                new StringBuilder("global java.util.List results;\n" +
+                        "import " + Person.class.getCanonicalName() + ";\n");
 
-        KieSession ksession = getKieSession(str);
+
+        rule.append(rulesOnLetter("a"));
+
+        KieSession ksession = getKieSession(rule.toString());
         ArrayList<Object> results = new ArrayList<>();
         ksession.setGlobal("results", results);
         Person a = new Person("a", 15);
@@ -85,5 +52,30 @@ public class LargeAlphaNetworkTest extends BaseModelTest {
         } finally {
             ksession.dispose();
         }
+    }
+
+    private String rulesOnLetter(final String letter) {
+        String quotedLetter = String.format("\"%s\"", letter);
+
+        return "rule minus18 when\n" +
+                "    $p : Person( name == " + quotedLetter + ", age < 18 )\n" +
+                "then\n" +
+                " results.add($p);\n" +
+                "end\n" +
+                "rule between1825 when\n" +
+                "    $p : Person( name == " + quotedLetter + ", age >= 18, age <= 25 )\n" +
+                "then\n" +
+                " results.add($p);\n" +
+                "end\n" +
+                "rule between2530 when\n" +
+                "    $p : Person( name == " + quotedLetter + ", age >= 25, age <= 30 )\n" +
+                "then\n" +
+                " results.add($p);\n" +
+                "end\n" +
+                "rule greaterThan30 when\n" +
+                "    $p : Person( name == " + quotedLetter + ", age > 30 )\n" +
+                "then\n" +
+                " results.add($p);\n" +
+                "end\n";
     }
 }
